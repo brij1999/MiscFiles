0b2d94310cbbe7524bdbcb5fa68b642a6851349a
Brijgopal Bharadwaj <36758947+Brijgopal@users.noreply.github.com>
2019-05-22 16:37:30 +0530

    Update myhttp.m

--- myhttp.m
+++ myhttp.m
@@ -1,15 +1,19 @@
 clc;
 clear all;
 
-rate = 10;
+mnths = {'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'};
+rate = 1;
 duration = 4;
-date = 20;
-month = 'May';
+date = 22;
+month = 5;
 year = 2019;
-hour = 09;
-minute = 00;
+hour = 11;
+minute = 19;
+
 urlwrite('http://srs2.cat.ernet.in:8100/servlet/Indus2BPIDataDownloadHA','data.zip','post',...
-    {'master',num2str(rate),'slave',num2str(duration),'DD1',num2str(date),'MMM1',month,'YYYY1',num2str(year),'HH1',num2str(hour),'MM1',num2str(minute)});
+    {'master',num2str(rate),'slave',num2str(duration),'DD1',num2str(date),'MMM1',...
+    mnths(month),'YYYY1',num2str(year),'HH1',num2str(hour),'MM1',num2str(minute)});
+
 file = unzip('data.zip');
 file = cell2mat(file);
 
