cea9af128d209a90698132f56246d28f01b32dec
Brijgopal Bharadwaj <36758947+Brijgopal@users.noreply.github.com>
2019-05-22 12:58:06 +0530

    Update myhttp.m

--- myhttp.m
+++ myhttp.m
@@ -1,15 +1,15 @@
 clc;
 clear all;
 
-rate = '10';
-duration = '4';
-date = '20';
+rate = 10;
+duration = 4;
+date = 20;
 month = 'May';
-year = '2019';
-hour = '09';
-minute = '00';
+year = 2019;
+hour = 09;
+minute = 00;
 urlwrite('http://srs2.cat.ernet.in:8100/servlet/Indus2BPIDataDownloadHA','data.zip','post',...
-    {'master',rate,'slave',duration,'DD1',date,'MMM1',month,'YYYY1',year,'HH1',hour,'MM1',minute});
+    {'master',num2str(rate),'slave',num2str(duration),'DD1',num2str(date),'MMM1',month,'YYYY1',num2str(year),'HH1',num2str(hour),'MM1',num2str(minute)});
 file = unzip('data.zip');
 file = cell2mat(file);
 
