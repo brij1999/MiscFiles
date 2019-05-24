a049d559bb69aed80ec632f326a5aa2cb315bc10
Brijgopal Bharadwaj <36758947+Brijgopal@users.noreply.github.com>
2019-05-24 10:24:10 +0530

    Update myhttp.m

--- myhttp.m
+++ myhttp.m
@@ -7,16 +7,26 @@ duration = 4;
 date = 22;
 month = 5;
 year = 2019;
-hour = 11;
-minute = 19;
 
-urlwrite('http://srs2.cat.ernet.in:8100/servlet/Indus2BPIDataDownloadHA','data.zip','post',...
+hr = 0;
+nrow = 1;
+
+while hr<24
+    pause(1);
+    
+    urlwrite('http://srs2.cat.ernet.in:8100/servlet/Indus2BPIDataDownloadHA','received.zip','post',...
     {'master',num2str(rate),'slave',num2str(duration),'DD1',num2str(date),'MMM1',...
-    mnths(month),'YYYY1',num2str(year),'HH1',num2str(hour),'MM1',num2str(minute)});
-
-file = unzip('data.zip');
-file = cell2mat(file);
-
-[num,txt,raw] = xlsread(file);
-
-delete(file);
+    mnths(month),'YYYY1',num2str(year),'HH1',num2str(hr),'MM1','00'});
+    
+    file = unzip('received.zip');
+    file = cell2mat(file);
+    [num,txt,raw] = xlsread(file);
+    delete(file);
+    nextr = ['A' num2str(nrow)];
+    xlswrite('data.xlsx', raw, 1, nextr);
+    
+    
+    s = size(raw);
+    nrow = nrow + s(1);
+    hr = hr + duration;
+end
